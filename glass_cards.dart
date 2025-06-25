23c0a499073f3aa5f0df94a57ac2063375158cbc
bright-ngantcha20 <jbraxy2005@gmail.com>
2025-06-25 17:18:09 +0100

    Create glass_cards.dart

---
+++ glass_cards.dart
@@ -0,0 +1,45 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class GlassCard extends StatelessWidget {
+  final Widget child;
+  final double borderRadius;
+  final double blur;
+  final double opacity;
+  final Color color;
+
+  const GlassCard({
+    super.key,
+    required this.child,
+    this.borderRadius = 32,
+    this.blur = 16,
+    this.opacity = 0.25,
+    this.color = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(borderRadius),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
+        child: Container(
+          decoration: BoxDecoration(
+            color: color.withOpacity(opacity),
+            borderRadius: BorderRadius.circular(borderRadius),
+            border:
+                Border.all(color: Colors.white.withOpacity(0.2), width: 1.5),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.08),
+                blurRadius: 16,
+                offset: Offset(0, 8),
+              ),
+            ],
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
